810977b41684f6734ca13a7a8dd178433297e895
Kirill Simonov <xi@resolvent.net>
2006-05-15 18:43:58 +0000

    Dynamically determine the inf and nan values. Should fix #14. Thanks to Scott Daniels for the report and the patch.

--- lib/yaml/representer.py
+++ lib/yaml/representer.py
@@ -192,9 +192,14 @@ class SafeRepresenter(BaseRepresenter):
     def represent_long(self, data):
         return self.represent_scalar(u'tag:yaml.org,2002:int', unicode(data))
 
-    repr_pos_inf = repr(1e300000)
-    repr_neg_inf = repr(-1e300000)
-    repr_nan = repr(1e300000/1e300000)
+    inf_value = 1e300
+    while repr(inf_value) != repr(inf_value*inf_value):
+        inf_value *= inf_value
+    nan_value = inf_value/inf_value
+
+    repr_pos_inf = repr(inf_value)
+    repr_neg_inf = repr(-inf_value)
+    repr_nan = repr(inf_value/inf_value)
 
     def represent_float(self, data):
         repr_data = repr(data)
