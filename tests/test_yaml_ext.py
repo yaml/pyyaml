6743cf6181c69ac3b08ff50ec6b4c8250fb5a101
Kirill Simonov <xi@resolvent.net>
2006-07-07 19:39:29 +0000

    Update PyRex based bindings to libyaml to include Parser functions.

--- tests/test_yaml_ext.py
+++ tests/test_yaml_ext.py
@@ -8,7 +8,7 @@ class TestExtVersion(unittest.TestCase):
     def testExtVersion(self):
         self.failUnlessEqual("%s.%s.%s" % _yaml.get_version(), _yaml.get_version_string())
 
-class TestExtScanner(test_appliance.TestAppliance):
+class TestExtLoader(test_appliance.TestAppliance):
 
     def _testExtScanner(self, test_name, data_filename, canonical_filename):
         data = file(data_filename, 'r').read()
@@ -34,7 +34,35 @@ class TestExtScanner(test_appliance.TestAppliance):
             print "EXT_TOKENS:", ext_tokens
             raise
 
-TestExtScanner.add_tests('testExtScanner', '.data', '.canonical')
+    def _testExtParser(self, test_name, data_filename, canonical_filename):
+        data = file(data_filename, 'r').read()
+        events = list(yaml.parse(data))
+        ext_events = []
+        try:
+            for event in yaml.parse(data, Loader=yaml.ExtLoader):
+                ext_events.append(event)
+                #print "EVENT:", event
+            self.failUnlessEqual(len(events), len(ext_events))
+            for event, ext_event in zip(events, ext_events):
+                self.failUnlessEqual(event.__class__, ext_event.__class__)
+                if hasattr(event, 'anchor'):
+                    self.failUnlessEqual(event.anchor, ext_event.anchor)
+                if hasattr(event, 'tag'):
+                    self.failUnlessEqual(event.tag, ext_event.tag)
+                if hasattr(event, 'implicit'):
+                    self.failUnlessEqual(event.implicit, ext_event.implicit)
+                if hasattr(event, 'value'):
+                    self.failUnlessEqual(event.value, ext_event.value)
+        except:
+            print
+            print "DATA:"
+            print file(data_filename, 'rb').read()
+            print "EVENTS:", events
+            print "EXT_EVENTS:", ext_events
+            raise
+
+TestExtLoader.add_tests('testExtScanner', '.data', '.canonical')
+TestExtLoader.add_tests('testExtParser', '.data', '.canonical')
 
 def main(module='__main__'):
     unittest.main(module)
