8f9b8bed40a53c285ebfd2613ef5af00d2fc6b97
Kirill Simonov <xi@resolvent.net>
2006-08-16 21:07:47 +0000

    The 'N' plain scalar was still recognized as ``!!bool``. Fix it (close #26).

--- lib/yaml/resolver.py
+++ lib/yaml/resolver.py
@@ -153,7 +153,7 @@ class Resolver(BaseResolver):
 
 Resolver.add_implicit_resolver(
         u'tag:yaml.org,2002:bool',
-        re.compile(ur'''^(?:yes|Yes|YES|n|N|no|No|NO
+        re.compile(ur'''^(?:yes|Yes|YES|no|No|NO
                     |true|True|TRUE|false|False|FALSE
                     |on|On|ON|off|Off|OFF)$''', re.X),
         list(u'yYnNtTfFoO'))
