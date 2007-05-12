e1d133ae334bf420a91fc49ffabe252bad833566
Kirill Simonov <xi@resolvent.net>
2007-05-12 21:27:19 +0000

    Update the package version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 NAME = 'PyYAML'
-VERSION = '3.04'
+VERSION = '3.05'
 DESCRIPTION = "YAML parser and emitter for Python"
 LONG_DESCRIPTION = """\
 YAML is a data serialization format designed for human readability and
