9027393a4f6c6a039aced0502cbce6eb40e68ad1
Kirill Simonov <xi@resolvent.net>
2008-12-05 19:37:52 +0000

    Added a comment on Python 3 support.

--- setup.py
+++ setup.py
@@ -26,10 +26,14 @@ CLASSIFIERS = [
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.3",
     "Programming Language :: Python :: 2.4",
     "Programming Language :: Python :: 2.5",
     "Programming Language :: Python :: 2.6",
+#   Python 3.0 is not yet supported, but see http://pyyaml.org/ticket/74
+#    "Programming Language :: Python :: 3",
+#    "Programming Language :: Python :: 3.0",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Text Processing :: Markup",
 ]
