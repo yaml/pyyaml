96ee4cbfcc0aadb73d46f06f0672b7b4d5a29fb6
Kirill Simonov <xi@resolvent.net>
2014-03-26 19:34:36 -0500

    Bumped the version number.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 NAME = 'PyYAML'
-VERSION = '3.10'
+VERSION = '3.11'
 DESCRIPTION = "YAML parser and emitter for Python"
 LONG_DESCRIPTION = """\
 YAML is a data serialization format designed for human readability
