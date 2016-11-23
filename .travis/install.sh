86a29eb50f26a877d08df317d225cd245fc66859
Ian Cordasco <graffatcolmingov@gmail.com>
2016-11-23 17:26:13 -0600

    Install tox in a virtualenv
    Since TravisCI has an extremely old version of PyPy, we need to use
    pyenv to install a more recent version. However, pyenv's shims don't
    persist between the install and script portions of Travis. So we have to
    use a virtualenv so Travis can find the tox command.

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,10 +8,10 @@ if [[ "${TOXENV}" = pypy* ]]; then
     pyenv install "pypy-$PYPY_VERSION"
     pyenv global "pypy-$PYPY_VERSION"
 
-    pip install --upgrade tox
-
     pyenv rehash
 fi
 
-
+pip install --upgrade virtualenv
+virtualenv ~/.venv
+source ~/.venv/bin/activate
 pip install --upgrade tox
