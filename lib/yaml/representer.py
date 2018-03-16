86a1f046ccec5703391202efc4c640f1f903a9fd
Tina Müller <cpan2@tinita.de>
2018-03-16 23:25:44 +0100

    Allow to turn off sorting keys in Dumper

--- lib/yaml/representer.py
+++ lib/yaml/representer.py
@@ -17,9 +17,10 @@ class BaseRepresenter(object):
     yaml_representers = {}
     yaml_multi_representers = {}
 
-    def __init__(self, default_style=None, default_flow_style=None):
+    def __init__(self, default_style=None, default_flow_style=None, sort_keys=True):
         self.default_style = default_style
         self.default_flow_style = default_flow_style
+        self.sort_keys = sort_keys
         self.represented_objects = {}
         self.object_keeper = []
         self.alias_key = None
@@ -117,7 +118,8 @@ class BaseRepresenter(object):
         best_style = True
         if hasattr(mapping, 'items'):
             mapping = mapping.items()
-            mapping.sort()
+            if self.sort_keys:
+                mapping.sort()
         for item_key, item_value in mapping:
             node_key = self.represent_data(item_key)
             node_value = self.represent_data(item_value)
