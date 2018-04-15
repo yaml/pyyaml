45dcdcc999ce7bb907d49cb7f3966e5730be4fba
Tina Müller <cpan2@tinita.de>
2018-04-15 14:46:24 +0200

    Support loading sequence mapping keys as tuples
    Support nested sequences as mapping keys

--- lib/yaml/constructor.py
+++ lib/yaml/constructor.py
@@ -11,7 +11,7 @@ __all__ = [
 from error import *
 from nodes import *
 
-import datetime
+import collections, datetime
 
 import binascii, re, sys, types
 
@@ -123,6 +123,19 @@ class BaseConstructor(object):
         return [self.construct_object(child, deep=deep)
                 for child in node.value]
 
+    def get_hashable_key(self, key):
+        try:
+            hash(key)
+        except:
+            if isinstance(key, list):
+                for i in range(len(key)):
+                    if not isinstance(key[i], collections.Hashable):
+                        key[i] = self.get_hashable_key(key[i])
+                key = tuple(key)
+                return key
+            raise Exception("unhashable key")
+        return key
+
     def construct_mapping(self, node, deep=False):
         if not isinstance(node, MappingNode):
             raise ConstructorError(None, None,
@@ -130,11 +143,12 @@ class BaseConstructor(object):
                     node.start_mark)
         mapping = {}
         for key_node, value_node in node.value:
-            key = self.construct_object(key_node, deep=deep)
-            try:
-                hash(key)
-            except TypeError, exc:
-                raise ConstructorError("while constructing a mapping", node.start_mark,
+            key = self.construct_object(key_node, deep=True)
+            if not isinstance(key, collections.Hashable):
+                try:
+                    key = self.get_hashable_key(key)
+                except Exception as exc:
+                    raise ConstructorError("while constructing a mapping", node.start_mark,
                         "found unacceptable key (%s)" % exc, key_node.start_mark)
             value = self.construct_object(value_node, deep=deep)
             mapping[key] = value
