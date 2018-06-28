67116ffc535be57499a44031a1057d79424d7397
Marty E. Plummer <hanetzer@startmail.com>
2018-06-28 04:59:38 -0500

    treewide: lib3->lib
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- setup.py
+++ setup.py
@@ -255,7 +255,7 @@ class test(Command):
         if sys.version_info[0] < 3:
             sys.path.insert(0, 'tests/lib2')
         else:
-            sys.path.insert(0, 'tests/lib3')
+            sys.path.insert(0, 'tests/lib')
         import test_all
         if not test_all.main([]):
             raise DistutilsError("Tests failed")
@@ -285,7 +285,7 @@ if __name__ == '__main__':
         download_url=DOWNLOAD_URL,
         classifiers=CLASSIFIERS,
 
-        package_dir={'': {2: 'lib2', 3: 'lib3'}[sys.version_info[0]]},
+        package_dir={'': {2: 'lib2', 3: 'lib'}[sys.version_info[0]]},
         packages=['yaml'],
         ext_modules=[
             Extension('_yaml', ['ext/_yaml.pyx'],
