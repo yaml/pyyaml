df440169701d25f75903b1b9b27c42d49c5a1ded
Ingy döt Net <ingy@ingy.net>
2018-07-02 16:49:52 -0700

    Release 3.13b1 updates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 NAME = 'PyYAML'
-VERSION = '3.12'
+VERSION = '3.13'
 DESCRIPTION = "YAML parser and emitter for Python"
 LONG_DESCRIPTION = """\
 YAML is a data serialization format designed for human readability
