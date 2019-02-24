b515dbf4f13d45e57bd276b3a915b22a8a9e0fbc
Matt Davis <nitzmahone@users.noreply.github.com>
2019-02-23 22:43:32 -0800

    Squash/merge pull request #105 from nnadeau/patch-1
    Removed Python 2.6 & 3.3

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ CLASSIFIERS = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.4",
@@ -299,5 +298,5 @@ if __name__ == '__main__':
 
         distclass=Distribution,
         cmdclass=cmdclass,
-        python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+        python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     )
