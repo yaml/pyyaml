a94428474424e75e438959b7ad9c0a396dece038
Ingy döt Net <ingy@ingy.net>
2019-02-24 17:22:01 -0800

    Updates for 5.1b1 release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 NAME = 'PyYAML'
-VERSION = '5.1'
+VERSION = '5.1b1'
 DESCRIPTION = "YAML parser and emitter for Python"
 LONG_DESCRIPTION = """\
 YAML is a data serialization format designed for human readability
