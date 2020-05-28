cb71ba55e957d700dba50fb658d90e559c44bc4b
Brad Solomon <bsolomon@protonmail.com>
2020-05-27 22:42:23 -0400

    Build MacOS wheels

---
+++ packaging/build/macos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+./packaging/build/libyaml.sh
+
+brew update
+brew upgrade pyenv
+eval "$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+pyenv --version
+pyenv install -s "$PYENV_VERSION"
+pyenv local "$PYENV_VERSION"
+python -V
+python -m pip install -U Cython wheel setuptools delocate
+python setup.py bdist_wheel
+delocate-listdeps dist/*.whl
+delocate-wheel -v dist/*.whl
+ls -1 dist/
