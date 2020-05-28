02de749a9df5306ca6e1838f704de89be386da31
Brad Solomon <bsolomon@protonmail.com>
2020-05-28 09:17:47 -0400

    Echo available Python versions if pyenv cannot find

--- packaging/build/macos.sh
+++ packaging/build/macos.sh
@@ -10,7 +10,9 @@ eval "$(pyenv init -)"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 pyenv --version
-pyenv install -s "$PYENV_VERSION"
+# If the Python version isn't found (e.g. it's not available in
+# share/python-build, then at least list what's available)
+pyenv install -s "$PYENV_VERSION" || (pyenv install --list && exit 1)
 pyenv local "$PYENV_VERSION"
 python -V
 pip install -U Cython wheel setuptools delocate
