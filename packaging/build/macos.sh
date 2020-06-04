c78106793ea38df4855af946387246974342d2fe
Brad Solomon <bsolomon@protonmail.com>
2020-06-04 19:07:54 -0400

    Put brew cmds in the right place

--- packaging/build/macos.sh
+++ packaging/build/macos.sh
@@ -2,9 +2,6 @@
 
 set -eux
 
-brew update -y
-brew upgrade gettext || brew install gettext
-brew upgrade automake || brew install automake
 python -V
 pip install -U setuptools wheel pip Cython delocate
 python setup.py bdist_wheel
