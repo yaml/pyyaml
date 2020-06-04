71fe365bd981ca4c114c5923e85342df75ec6d3b
Brad Solomon <bsolomon@protonmail.com>
2020-06-04 19:04:08 -0400

    Fixups for GitHub Actions build environment

--- packaging/build/macos.sh
+++ packaging/build/macos.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+brew update -y
+brew upgrade gettext || brew install gettext
+brew upgrade automake || brew install automake
 python -V
 pip install -U setuptools wheel pip Cython delocate
 python setup.py bdist_wheel
