4f9aae0567ff336441cae0c5c68ecbec8e519f32
Brad Solomon <bsolomon@protonmail.com>
2020-06-04 18:35:54 -0400

    Convert wheel build from Travis CI to GitHub Actions

--- packaging/build/macos.sh
+++ packaging/build/macos.sh
@@ -2,20 +2,8 @@
 
 set -eux
 
-./packaging/build/libyaml.sh
-
-brew update
-brew upgrade pyenv
-eval "$(pyenv init -)"
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-pyenv --version
-# If the Python version isn't found (e.g. it's not available in
-# share/python-build, then at least list what's available)
-pyenv install -s "$PYENV_VERSION" || (pyenv install --list && exit 1)
-pyenv local "$PYENV_VERSION"
 python -V
-pip install -U Cython wheel setuptools delocate
+pip install -U setuptools wheel pip Cython delocate
 python setup.py bdist_wheel
 delocate-listdeps dist/*.whl
 delocate-wheel -v dist/*.whl
--- packaging/build/manylinux.sh
+++ packaging/build/manylinux.sh
@@ -2,25 +2,30 @@
 
 set -eux
 
+# We need to build this here rather than as a CI step
+# so that it's easily available in the container itself
 ./packaging/build/libyaml.sh
 
 mkdir -p wheelhouse
 rm -vf wheelhouse/*.whl
 
 # PyYAML supports Python 2.7, 3.5-3.8
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/python" -m pip install -U Cython auditwheel wheel setuptools pip
-    "${PYBIN}/python" -m pip wheel \
-        --verbose \
-        --no-deps \
-        --global-option '--with-libyaml' \
-        --global-option "build_ext" \
-        -w wheelhouse .
+for tag in $PYTHON_TAGS; do
+  PYBIN="/opt/python/${tag}/bin"
+  "${PYBIN}/python" -m pip install -U wheel setuptools pip Cython auditwheel
+  "${PYBIN}/python" -m pip wheel \
+    --verbose \
+    --no-deps \
+    --global-option '--with-libyaml' \
+    --global-option "build_ext" \
+    -w wheelhouse .
 done
 
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat "$PLAT" -w wheelhouse/
-    rm -f "$whl"
+  auditwheel repair "$whl" --plat "$PLAT" -w wheelhouse/
+  rm -f "$whl"
 done
 
-ls -1 wheelhouse/
+mkdir dist
+mv wheelhouse/*.whl dist/
+ls -1 dist/
