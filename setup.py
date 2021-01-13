5bda41909a4d93062bacaf48df599dbf0cb4cf1e
Matt Davis <mrd@redhat.com>
2021-01-13 15:57:08 -0800

    5.4.0b1 release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 NAME = 'PyYAML'
-VERSION = '5.4.0a0'
+VERSION = '5.4b1'
 DESCRIPTION = "YAML parser and emitter for Python"
 LONG_DESCRIPTION = """\
 YAML is a data serialization format designed for human readability
