00be495c7890efa7a34b69ad526637fe9b587b40
Thom Smith <thom@thomthesmith.com>
2021-09-02 16:47:31 -0400

    Simplify python_requires

--- setup.py
+++ setup.py
@@ -287,5 +287,5 @@ if __name__ == '__main__':
 
         distclass=Distribution,
         cmdclass=cmdclass,
-        python_requires='!=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+        python_requires='>=3.6',
     )
