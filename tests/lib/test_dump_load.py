1b2f5343e5395e9c871d01b42e1158db34a4713a
Ingy döt Net <ingy@ingy.net>
2021-09-23 14:25:27 -0700

    Add a test to assert load(s) fails

--- tests/lib/test_dump_load.py
+++ tests/lib/test_dump_load.py
@@ -4,6 +4,15 @@ def test_dump(verbose=False):
     assert yaml.dump(['foo'])
 test_dump.unittest = True
 
+def test_load_no_loader(verbose=False):
+    try:
+        yaml.load("- foo\n")
+    except TypeError:
+        return True
+    assert(False, "load() require Loader=...")
+
+test_load_no_loader.unittest = True
+
 def test_load_safeloader(verbose=False):
     assert yaml.load("- foo\n", Loader=yaml.SafeLoader)
 test_load_safeloader.unittest = True
