af0275b1477c826a2938568a896454556d18e855
Ingy döt Net <ingy@ingy.net>
2021-09-23 13:46:48 -0700

    The yaml.load{,_all} functions require Loader= now

--- tests/lib/test_dump_load.py
+++ tests/lib/test_dump_load.py
@@ -4,10 +4,6 @@ def test_dump(verbose=False):
     assert yaml.dump(['foo'])
 test_dump.unittest = True
 
-def test_load(verbose=False):
-    assert yaml.load("- foo\n")
-test_load.unittest = True
-
 def test_load_safeloader(verbose=False):
     assert yaml.load("- foo\n", Loader=yaml.SafeLoader)
 test_load_safeloader.unittest = True
