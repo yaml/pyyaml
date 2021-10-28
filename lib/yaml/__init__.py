6a603f2978cc141760d1f2b7247cefb9ee186412
adehad <26027314+adehad@users.noreply.github.com>
2021-10-29 00:42:43 +0100

    black autoformat

--- lib/yaml/__init__.py
+++ lib/yaml/__init__.py
@@ -1,4 +1,3 @@
-
 from .error import *
 
 from .tokens import *
@@ -8,24 +7,26 @@ from .nodes import *
 from .loader import *
 from .dumper import *
 
-__version__ = '6.0'
+__version__ = "6.0"
 try:
     from .cyaml import *
+
     __with_libyaml__ = True
 except ImportError:
     __with_libyaml__ = False
 
 import io
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # XXX "Warnings control" is now deprecated. Leaving in the API function to not
 # break code that uses it.
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 def warnings(settings=None):
     if settings is None:
         return {}
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def scan(stream, Loader=Loader):
     """
     Scan a YAML stream and produce scanning tokens.
@@ -36,6 +37,7 @@ def scan(stream, Loader=Loader):
             yield loader.get_token()
     finally:
         loader.dispose()
+
 
 def parse(stream, Loader=Loader):
     """
@@ -48,6 +50,7 @@ def parse(stream, Loader=Loader):
     finally:
         loader.dispose()
 
+
 def compose(stream, Loader=Loader):
     """
     Parse the first YAML document in a stream
@@ -58,6 +61,7 @@ def compose(stream, Loader=Loader):
         return loader.get_single_node()
     finally:
         loader.dispose()
+
 
 def compose_all(stream, Loader=Loader):
     """
@@ -71,6 +75,7 @@ def compose_all(stream, Loader=Loader):
     finally:
         loader.dispose()
 
+
 def load(stream, Loader):
     """
     Parse the first YAML document in a stream
@@ -81,6 +86,7 @@ def load(stream, Loader):
         return loader.get_single_data()
     finally:
         loader.dispose()
+
 
 def load_all(stream, Loader):
     """
@@ -94,6 +100,7 @@ def load_all(stream, Loader):
     finally:
         loader.dispose()
 
+
 def full_load(stream):
     """
     Parse the first YAML document in a stream
@@ -103,6 +110,7 @@ def full_load(stream):
     unsafe on untrusted input.
     """
     return load(stream, FullLoader)
+
 
 def full_load_all(stream):
     """
@@ -114,6 +122,7 @@ def full_load_all(stream):
     """
     return load_all(stream, FullLoader)
 
+
 def safe_load(stream):
     """
     Parse the first YAML document in a stream
@@ -123,6 +132,7 @@ def safe_load(stream):
     to be safe for untrusted input.
     """
     return load(stream, SafeLoader)
+
 
 def safe_load_all(stream):
     """
@@ -134,6 +144,7 @@ def safe_load_all(stream):
     """
     return load_all(stream, SafeLoader)
 
+
 def unsafe_load(stream):
     """
     Parse the first YAML document in a stream
@@ -143,6 +154,7 @@ def unsafe_load(stream):
     unsafe on untrusted input.
     """
     return load(stream, UnsafeLoader)
+
 
 def unsafe_load_all(stream):
     """
@@ -154,9 +166,17 @@ def unsafe_load_all(stream):
     """
     return load_all(stream, UnsafeLoader)
 
-def emit(events, stream=None, Dumper=Dumper,
-        canonical=None, indent=None, width=None,
-        allow_unicode=None, line_break=None):
+
+def emit(
+    events,
+    stream=None,
+    Dumper=Dumper,
+    canonical=None,
+    indent=None,
+    width=None,
+    allow_unicode=None,
+    line_break=None,
+):
     """
     Emit YAML parsing events into a stream.
     If stream is None, return the produced string instead.
@@ -165,8 +185,14 @@ def emit(events, stream=None, Dumper=Dumper,
     if stream is None:
         stream = io.StringIO()
         getvalue = stream.getvalue
-    dumper = Dumper(stream, canonical=canonical, indent=indent, width=width,
-            allow_unicode=allow_unicode, line_break=line_break)
+    dumper = Dumper(
+        stream,
+        canonical=canonical,
+        indent=indent,
+        width=width,
+        allow_unicode=allow_unicode,
+        line_break=line_break,
+    )
     try:
         for event in events:
             dumper.emit(event)
@@ -175,11 +201,22 @@ def emit(events, stream=None, Dumper=Dumper,
     if getvalue:
         return getvalue()
 
-def serialize_all(nodes, stream=None, Dumper=Dumper,
-        canonical=None, indent=None, width=None,
-        allow_unicode=None, line_break=None,
-        encoding=None, explicit_start=None, explicit_end=None,
-        version=None, tags=None):
+
+def serialize_all(
+    nodes,
+    stream=None,
+    Dumper=Dumper,
+    canonical=None,
+    indent=None,
+    width=None,
+    allow_unicode=None,
+    line_break=None,
+    encoding=None,
+    explicit_start=None,
+    explicit_end=None,
+    version=None,
+    tags=None,
+):
     """
     Serialize a sequence of representation trees into a YAML stream.
     If stream is None, return the produced string instead.
@@ -191,10 +228,19 @@ def serialize_all(nodes, stream=None, Dumper=Dumper,
         else:
             stream = io.BytesIO()
         getvalue = stream.getvalue
-    dumper = Dumper(stream, canonical=canonical, indent=indent, width=width,
-            allow_unicode=allow_unicode, line_break=line_break,
-            encoding=encoding, version=version, tags=tags,
-            explicit_start=explicit_start, explicit_end=explicit_end)
+    dumper = Dumper(
+        stream,
+        canonical=canonical,
+        indent=indent,
+        width=width,
+        allow_unicode=allow_unicode,
+        line_break=line_break,
+        encoding=encoding,
+        version=version,
+        tags=tags,
+        explicit_start=explicit_start,
+        explicit_end=explicit_end,
+    )
     try:
         dumper.open()
         for node in nodes:
@@ -205,6 +251,7 @@ def serialize_all(nodes, stream=None, Dumper=Dumper,
     if getvalue:
         return getvalue()
 
+
 def serialize(node, stream=None, Dumper=Dumper, **kwds):
     """
     Serialize a representation tree into a YAML stream.
@@ -212,12 +259,25 @@ def serialize(node, stream=None, Dumper=Dumper, **kwds):
     """
     return serialize_all([node], stream, Dumper=Dumper, **kwds)
 
-def dump_all(documents, stream=None, Dumper=Dumper,
-        default_style=None, default_flow_style=False,
-        canonical=None, indent=None, width=None,
-        allow_unicode=None, line_break=None,
-        encoding=None, explicit_start=None, explicit_end=None,
-        version=None, tags=None, sort_keys=True):
+
+def dump_all(
+    documents,
+    stream=None,
+    Dumper=Dumper,
+    default_style=None,
+    default_flow_style=False,
+    canonical=None,
+    indent=None,
+    width=None,
+    allow_unicode=None,
+    line_break=None,
+    encoding=None,
+    explicit_start=None,
+    explicit_end=None,
+    version=None,
+    tags=None,
+    sort_keys=True,
+):
     """
     Serialize a sequence of Python objects into a YAML stream.
     If stream is None, return the produced string instead.
@@ -229,12 +289,22 @@ def dump_all(documents, stream=None, Dumper=Dumper,
         else:
             stream = io.BytesIO()
         getvalue = stream.getvalue
-    dumper = Dumper(stream, default_style=default_style,
-            default_flow_style=default_flow_style,
-            canonical=canonical, indent=indent, width=width,
-            allow_unicode=allow_unicode, line_break=line_break,
-            encoding=encoding, version=version, tags=tags,
-            explicit_start=explicit_start, explicit_end=explicit_end, sort_keys=sort_keys)
+    dumper = Dumper(
+        stream,
+        default_style=default_style,
+        default_flow_style=default_flow_style,
+        canonical=canonical,
+        indent=indent,
+        width=width,
+        allow_unicode=allow_unicode,
+        line_break=line_break,
+        encoding=encoding,
+        version=version,
+        tags=tags,
+        explicit_start=explicit_start,
+        explicit_end=explicit_end,
+        sort_keys=sort_keys,
+    )
     try:
         dumper.open()
         for data in documents:
@@ -245,12 +315,14 @@ def dump_all(documents, stream=None, Dumper=Dumper,
     if getvalue:
         return getvalue()
 
+
 def dump(data, stream=None, Dumper=Dumper, **kwds):
     """
     Serialize a Python object into a YAML stream.
     If stream is None, return the produced string instead.
     """
     return dump_all([data], stream, Dumper=Dumper, **kwds)
+
 
 def safe_dump_all(documents, stream=None, **kwds):
     """
@@ -260,6 +332,7 @@ def safe_dump_all(documents, stream=None, **kwds):
     """
     return dump_all(documents, stream, Dumper=SafeDumper, **kwds)
 
+
 def safe_dump(data, stream=None, **kwds):
     """
     Serialize a Python object into a YAML stream.
@@ -268,8 +341,8 @@ def safe_dump(data, stream=None, **kwds):
     """
     return dump_all([data], stream, Dumper=SafeDumper, **kwds)
 
-def add_implicit_resolver(tag, regexp, first=None,
-        Loader=None, Dumper=Dumper):
+
+def add_implicit_resolver(tag, regexp, first=None, Loader=None, Dumper=Dumper):
     """
     Add an implicit scalar detector.
     If an implicit scalar value matches the given regexp,
@@ -283,6 +356,7 @@ def add_implicit_resolver(tag, regexp, first=None,
     else:
         Loader.add_implicit_resolver(tag, regexp, first)
     Dumper.add_implicit_resolver(tag, regexp, first)
+
 
 def add_path_resolver(tag, path, kind=None, Loader=None, Dumper=Dumper):
     """
@@ -299,6 +373,7 @@ def add_path_resolver(tag, path, kind=None, Loader=None, Dumper=Dumper):
         Loader.add_path_resolver(tag, path, kind)
     Dumper.add_path_resolver(tag, path, kind)
 
+
 def add_constructor(tag, constructor, Loader=None):
     """
     Add a constructor for the given tag.
@@ -311,6 +386,7 @@ def add_constructor(tag, constructor, Loader=None):
         loader.UnsafeLoader.add_constructor(tag, constructor)
     else:
         Loader.add_constructor(tag, constructor)
+
 
 def add_multi_constructor(tag_prefix, multi_constructor, Loader=None):
     """
@@ -326,6 +402,7 @@ def add_multi_constructor(tag_prefix, multi_constructor, Loader=None):
     else:
         Loader.add_multi_constructor(tag_prefix, multi_constructor)
 
+
 def add_representer(data_type, representer, Dumper=Dumper):
     """
     Add a representer for the given type.
@@ -334,6 +411,7 @@ def add_representer(data_type, representer, Dumper=Dumper):
     and producing the corresponding representation node.
     """
     Dumper.add_representer(data_type, representer)
+
 
 def add_multi_representer(data_type, multi_representer, Dumper=Dumper):
     """
@@ -344,13 +422,15 @@ def add_multi_representer(data_type, multi_representer, Dumper=Dumper):
     """
     Dumper.add_multi_representer(data_type, multi_representer)
 
+
 class YAMLObjectMetaclass(type):
     """
     The metaclass for YAMLObject.
     """
+
     def __init__(cls, name, bases, kwds):
         super(YAMLObjectMetaclass, cls).__init__(name, bases, kwds)
-        if 'yaml_tag' in kwds and kwds['yaml_tag'] is not None:
+        if "yaml_tag" in kwds and kwds["yaml_tag"] is not None:
             if isinstance(cls.yaml_loader, list):
                 for loader in cls.yaml_loader:
                     loader.add_constructor(cls.yaml_tag, cls.from_yaml)
@@ -358,6 +438,7 @@ class YAMLObjectMetaclass(type):
                 cls.yaml_loader.add_constructor(cls.yaml_tag, cls.from_yaml)
 
             cls.yaml_dumper.add_representer(cls, cls.to_yaml)
+
 
 class YAMLObject(metaclass=YAMLObjectMetaclass):
     """
@@ -385,6 +466,6 @@ class YAMLObject(metaclass=YAMLObjectMetaclass):
         """
         Convert a Python object to a representation node.
         """
-        return dumper.represent_yaml_object(cls.yaml_tag, data, cls,
-                flow_style=cls.yaml_flow_style)
-
+        return dumper.represent_yaml_object(
+            cls.yaml_tag, data, cls, flow_style=cls.yaml_flow_style
+        )
--- lib/yaml/loader.py
+++ lib/yaml/loader.py
@@ -1,5 +1,4 @@
-
-__all__ = ['BaseLoader', 'FullLoader', 'SafeLoader', 'Loader', 'UnsafeLoader']
+__all__ = ["BaseLoader", "FullLoader", "SafeLoader", "Loader", "UnsafeLoader"]
 
 from .reader import *
 from .scanner import *
@@ -8,8 +7,8 @@ from .composer import *
 from .constructor import *
 from .resolver import *
 
-class BaseLoader(Reader, Scanner, Parser, Composer, BaseConstructor, BaseResolver):
 
+class BaseLoader(Reader, Scanner, Parser, Composer, BaseConstructor, BaseResolver):
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)
@@ -18,8 +17,8 @@ class BaseLoader(Reader, Scanner, Parser, Composer, BaseConstructor, BaseResolve
         BaseConstructor.__init__(self)
         BaseResolver.__init__(self)
 
-class FullLoader(Reader, Scanner, Parser, Composer, FullConstructor, Resolver):
 
+class FullLoader(Reader, Scanner, Parser, Composer, FullConstructor, Resolver):
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)
@@ -28,8 +27,8 @@ class FullLoader(Reader, Scanner, Parser, Composer, FullConstructor, Resolver):
         FullConstructor.__init__(self)
         Resolver.__init__(self)
 
-class SafeLoader(Reader, Scanner, Parser, Composer, SafeConstructor, Resolver):
 
+class SafeLoader(Reader, Scanner, Parser, Composer, SafeConstructor, Resolver):
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)
@@ -38,8 +37,8 @@ class SafeLoader(Reader, Scanner, Parser, Composer, SafeConstructor, Resolver):
         SafeConstructor.__init__(self)
         Resolver.__init__(self)
 
-class Loader(Reader, Scanner, Parser, Composer, Constructor, Resolver):
 
+class Loader(Reader, Scanner, Parser, Composer, Constructor, Resolver):
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)
@@ -48,12 +47,12 @@ class Loader(Reader, Scanner, Parser, Composer, Constructor, Resolver):
         Constructor.__init__(self)
         Resolver.__init__(self)
 
+
 # UnsafeLoader is the same as Loader (which is and was always unsafe on
 # untrusted input). Use of either Loader or UnsafeLoader should be rare, since
 # FullLoad should be able to load almost all YAML safely. Loader is left intact
 # to ensure backwards compatibility.
 class UnsafeLoader(Reader, Scanner, Parser, Composer, Constructor, Resolver):
-
     def __init__(self, stream):
         Reader.__init__(self, stream)
         Scanner.__init__(self)
