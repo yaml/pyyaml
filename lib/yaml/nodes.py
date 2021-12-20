41b3415e41da79fe4605cf891a89877c789a1456
Ebram Shehata <ebram96@gmail.com>
2021-12-20 17:29:24 +0200

    Update nodes.py
    Remove commented code.

--- lib/yaml/nodes.py
+++ lib/yaml/nodes.py
@@ -6,20 +6,7 @@ class Node(object):
         self.start_mark = start_mark
         self.end_mark = end_mark
     def __repr__(self):
-        value = self.value
-        #if isinstance(value, list):
-        #    if len(value) == 0:
-        #        value = '<empty>'
-        #    elif len(value) == 1:
-        #        value = '<1 item>'
-        #    else:
-        #        value = '<%d items>' % len(value)
-        #else:
-        #    if len(value) > 75:
-        #        value = repr(value[:70]+u' ... ')
-        #    else:
-        #        value = repr(value)
-        value = repr(value)
+        value = repr(self.value)
         return '%s(tag=%r, value=%s)' % (self.__class__.__name__, self.tag, value)
 
 class ScalarNode(Node):
